54021ccd0e19eb715f0596e3a43c9496a0b77a1e
unknown <phayvorononobi@gmail.com>
2023-08-17 02:14:27 -1100

    update:_)

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <div className='w-[100vw] h-[50px] flex justify-between'>
       <h1 className='text-white-500 text-[30px] m-3 font-extrabold text-xl'>Portfolio</h1>
-      <button className='w-[100px] h-[30px] border-green-500 mt-2 mr-1 border-2 rounded-md hover: bg-green-500 text-white-500'>RESUME</button>
+      <button className='w-[100px] h-[30px] mt-2 mr-1 border-2 border-none rounded-md bg-green-500 text-white-500'>RESUME</button>
     </div>
   )
 }
