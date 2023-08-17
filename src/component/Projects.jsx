18ee5050df950d25694185c952807f24e7bc248e
unknown <phayvorononobi@gmail.com>
2023-08-17 02:19:47 -1100

    projects

---
+++ src/component/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='flex items-center justify-center mt-[3rem]'>
+        <h1 className='text-[40px] font-bold border-r-2 border-green-500 w-[100vw] py-5 text-center'>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
