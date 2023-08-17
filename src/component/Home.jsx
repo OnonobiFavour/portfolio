f4a101c0ae1b562b78839d5c1257ac35811a7edf
unknown <phayvorononobi@gmail.com>
2023-08-17 02:11:51 -1100

    update:_)

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Nav from './Nav'
 import About from './About'
+import Projects from './Projects'
 
 const Home = () => {
   return (
     <section className='h-full w-full bg-[#00100d] text-violet-50'>
       <Nav/>
-      <div className='flex justify-center items-center flex-wrap md: justiify-around mt-12 ml-6'>
+      <header className='flex justify-center items-center flex-wrap mt-[5rem] md: justiify-around ml-6'>
         <div className='flex flex-col '>
           <span className='text-[30px] font-[500] py-3'>Crafting Creativity: The journey..</span>
           <h1 className='text-[20px] w-[320px] font-[100] pb-5'>Welcome to my creative universe! i'm Ononobi Favour, a frontend developer and a technical writer</h1>
@@ -14,9 +15,10 @@ const Home = () => {
         <div className='w-[200px] ml-5 h-[200px] border-green-600 border-2 rounded-full mr-[2.5rem]'>
           <img className='rounded-full' src="../src/assets/profilePics.jpg" alt="" />
         </div>
-      </div>
+      </header>
 
       <About />
+      <Projects />
     </section> 
   )
 }
