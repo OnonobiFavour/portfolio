a44105aecee58414abcc6cac381e556e198159e1
unknown <phayvorononobi@gmail.com>
2023-08-28 14:07:56 -1100

    new Addition

---
+++ src/component/JS/Projects.js
@@ -0,0 +1,30 @@
+export const project =[
+    {
+        id: 1,
+        head: 'Counter Web APP',
+        msg: 'This app was created with Vuejs to implement counts consisting of an Increment, decrement and reset Value function.',
+        code: 'https://github.com/OnonobiFavour/Altschool-vue-counterApp',
+        host: 'https://altschool-vue-counter-app.vercel.app',
+    },
+    {
+        id: 2,
+        head: 'Counter Web App using UseReducer hook',
+        msg: 'This app was created with React using the useReducer hook to implement the counts.',
+        code: 'https://github.com/OnonobiFavour/Use-Reducer-Counter',
+        host: 'http://reducer-count-by-favor.vercel.app/'
+    },
+    {
+        id: 2,
+        head: 'USSY',
+        msg: 'This app was created with React using the useReducer hook to implement the counts.',
+        code: 'https://github.com/OnonobiFavour/Ussy',
+        host: 'http://reducer-count-by-favor.vercel.app/'
+    },
+    {
+        id: 2,
+        head: 'Counter Web App using UseReducer hook',
+        msg: 'This app was created with React using the useReducer hook to implement the counts.',
+        code: 'https://github.com/OnonobiFavour/Use-Reducer-Counter',
+        host: 'http://reducer-count-by-favor.vercel.app/'
+    }
+]
