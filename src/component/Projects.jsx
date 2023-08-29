4b0df1a2a450acc2fdf3f6cb08c964cb16885018
unknown <phayvorononobi@gmail.com>
2023-08-28 14:15:55 -1100

    my commit

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {project} from './Js/Projects'
 
 const Projects = () => {
   return (
-    <div className='flex items-center justify-center mt-[3rem]'>
+    <div className='flex items-center justify-center flex-col mt-[3rem]'>
         <h1 className='text-[40px] font-bold border-r-2 border-green-500 w-[100vw] py-5 text-center'>Projects</h1>
+        <main>
+            {
+                project.map((item, index) =>{
+                    return(
+                        <div key={index} className='w-[300px] h-[200px] bg-black shadow-xl rounded-md px-5 pt-4 my-[2rem]'>
+                            <h1 className='text-[20px] pb-3'>{item.head}</h1>
+                            <span>{item.msg}</span> <br />
+                            <a href={item.code} className='text-green-500 pt-2'>Browse Code{'>>'}</a>
+                        </div>
+                    )
+                })
+            }
+        </main>
     </div>
   )
 }
